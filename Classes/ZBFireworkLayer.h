11388b2cdf2f98806513e73e5d03b63f193470c5
zonble <zonble@gmail.com>
2017-02-03 01:51:15 +0800

    Fixes typo.

--- Classes/ZBFireworkLayer.h
+++ Classes/ZBFireworkLayer.h
@@ -3,6 +3,6 @@
 
 @interface ZBFireworkLayer : CALayer
 
-- (instancetype)ininstancetypeWithHue:(CGFloat)inHue NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithHue:(CGFloat)inHue NS_DESIGNATED_INITIALIZER;
 - (void)animateInLayer:(CALayer *)inSuperlayer from:(CGPoint)inFrom to:(CGPoint)inTo;
 @end
